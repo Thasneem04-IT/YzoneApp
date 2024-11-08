ab9758bbf657416783cb1a11c9891a4d2c42bc83
Thasneem Sulthana A <thasneem.yzone@gmail.com>
2024-11-08 12:47:17 +0000

    yzone-quiz

--- pages/Page1/jsobjects/JSObject1/JSObject1.js
+++ pages/Page1/jsobjects/JSObject1/JSObject1.js
@@ -1,5 +1,5 @@
 export default {
-	Button3onClick () {
-		
-	}
-}
+  initializeIndex: async () => {
+    await storeValue("currentIndex", 0);
+  }
+};
---
+++ pages/Page1/jsobjects/JSObject2/JSObject2.js
@@ -0,0 +1,6 @@
+export default {
+	Button9onClick () {
+   {{ storeValue("selectedStudent", List1.selectedItem) }}
+		return retrieve.data[0]
+	}
+}
